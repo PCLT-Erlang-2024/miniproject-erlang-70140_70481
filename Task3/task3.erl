d34c79dd23635cee7a1d89713b4d4714f293e230
VascoJ2000 <vascojoao2000escola@gmail.com>
2024-11-25 17:40:37 +0000

    Task 3 Complete

--- Task3/task3.erl
+++ Task3/task3.erl
@@ -1,19 +1,25 @@
 %% File: task3.erl
 
--module(task1).
+-module(task3).
 -export([start/0, feeder/2, conveyor/2, truck/1]).
 
 start() ->
-    TruckID = spawn(?MODULE, truck, ["Truck 0"]),
-    ConveyorID = spawn(?MODULE, conveyor, ["Conveyor 0", TruckID]),
-    spawn(?MODULE, feeder, ["Fedder 0", ConveyorID]).
+    Truck0ID = spawn(?MODULE, truck, ["Truck 0"]),
+    Conveyor0ID = spawn(?MODULE, conveyor, ["Conveyor 0", Truck0ID]),
+    spawn(?MODULE, feeder, ["Fedder 0", Conveyor0ID]),
+    Truck1ID = spawn(?MODULE, truck, ["Truck 1"]),
+    Conveyor1ID = spawn(?MODULE, conveyor, ["Conveyor 1", Truck1ID]),
+    spawn(?MODULE, feeder, ["Fedder 1", Conveyor1ID]),
+    Truck2ID = spawn(?MODULE, truck, ["Truck 2"]),
+    Conveyor2ID = spawn(?MODULE, conveyor, ["Conveyor 2", Truck2ID]),
+    spawn(?MODULE, feeder, ["Fedder 2", Conveyor2ID]).
 
 feeder(Name, Conveyor) ->
     io:format("~p: Started generating packages.~n", [Name]),
     feederLoop(Name, Conveyor, 0).
 
 feederLoop(Name, Conveyor, Counter) ->
-    Package = {package, Counter, 1}, % Generate package.
+    Package = {package, Counter, rand:uniform(10)}, % Generate package.
     io:format("~p: generated ~p~n", [Name, Package]),
     Conveyor ! {self(), Package},
     receive
@@ -34,26 +40,29 @@ conveyor(Name, Truck) ->
     io:format("~p: Started.~n", [Name]),
     beltLoop(Name, Truck, []).
 
-beltLoop(Name, Truck, List) ->
+beltLoop(Name, Truck, []) ->
     receive
-      {Fedder, {package, Counter, Size}} ->
-        Truck ! {self(), {package, Counter, Size}},
-        receive
-        {pause} ->
-            io:format("~p: Paused package generation.~n", [Name]),
-            Fedder ! {pause},
+        {Fedder, {package, Counter, Size}} ->
+            Truck ! {self(), {package, Counter, Size}},
+            io:format("~p: Package unloaded.~n", [Name]),
             receive
-            {resume} ->
-                io:format("~p: Resumed package generation.~n", [Name]),
-                Fedder ! {resume},
-                beltLoop(Name, Truck, List)
+            {pause} ->
+                io:format("~p: Paused package generation.~n", [Name]),
+                Fedder ! {pause},
+                receive
+                {resume} ->
+                    io:format("~p: Resumed package generation.~n", [Name]),
+                    Fedder ! {resume},
+                    beltLoop(Name, Truck, [])
+                end
+            after 1000 ->
+                io:format("~p: Package unloaded.~n", [Name]),
+                ok
             end
         after 1000 ->
-            io:format("~p: Package unloaded.~n", [Name]),
             ok
-        end
     end,
-    beltLoop(Name, Truck, List).
+    beltLoop(Name, Truck, []).
 
 truck(Name) ->
     TruckCapacity = 100,
@@ -63,10 +72,20 @@ truck(Name) ->
 truckLoop(Name, Capacity, Load) ->
     receive
         {Belt, {package, Counter, Size}} ->
-            Load ++ [{package, Counter, Size}]
-            if Capacity - Size == 0 ->
-                Belt ! {pause}
-        after 1000 ->
-            ok
-    end,
-    truckLoop(Name, Capacity, Load).
+            io:format("~p: Package Loaded.~n", [Name]),
+            NewCapacity = Capacity - Size,
+            if 
+                NewCapacity < 0 ->
+                    io:format("~p: Full! Leaving...~n", [Name]),
+                    Belt ! {pause},
+                    timer:sleep(rand:uniform(500)),
+                    io:format("~p: New truck arrived.~n", [Name]),
+                    Belt ! {resume},
+                    truckLoop(Name, 100-Size, [Size]);
+                true ->
+                    io:format("~p: Remaning capacity ~p.~n", [Name, NewCapacity]),
+                    truckLoop(Name, NewCapacity, Load ++ [{package, Counter, Size}])
+            end
+        after 1500 ->
+            truckLoop(Name, Capacity, Load)
+    end.
